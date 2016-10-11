c9999c73f82f8b3af9bce46a0e44dd8eb4d63fa4
Alishabegum <begumalisha98@gmail.com>
2016-10-11 12:48:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-1',function (req,res) {
+    res.send("this is the first article...}}");
+});
+app.get('/article-2',function(req,res){
+    res.send("this is the second article...}}");
+});
+app.get('/article-3',function(req,res){
+    res.send("this is the third article...}}");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
